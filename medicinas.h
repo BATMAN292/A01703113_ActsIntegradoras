80e64c993515c279f83c9ed44bfa66e1028e835a
BATMAN292 <89033091+BATMAN292@users.noreply.github.com>
2021-09-20 08:02:48 -0500

    Update medicinas.h

--- medicinas.h
+++ medicinas.h
@@ -1,1 +1,26 @@
-/** El códgo del main aquí **/
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <list>
+
+class Archivo{
+  public:
+    string linea
+    string texto
+
+    void leerArchivo();
+
+};
+
+void Archivo::leerArchivo(){
+  ifstream archivo("medicinas.csv");
+
+  while (getline(archivo)){
+    texto = texto + linea + "\n";
+  }
+
+  cout << texto << endl;
+}
